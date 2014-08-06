07b9bca326fd4945946e01335b78be353b88bea0
Carolyn Huynh <ckhuynh20@gmail.com>
2014-08-05 22:49:57 -0700

    shows the name of the contact

--- spec/sm_spec.rb
+++ spec/sm_spec.rb
@@ -3,8 +3,13 @@ require 'profile'
 require 'username'
 
 describe Profile do
-	it 'initalizes the name of contact with an empty array' do
+	it 'initalizes the name of contact with an empty array for their Twitter username' do
 		test_contact = Profile.new({:name => "Carolyn Huynh"})
 		test_contact.should be_an_instance_of Profile
+	end
+
+	it 'shows the name of the contact' do
+		test_contact = Profile.new({:name => "Carolyn Huynh"})
+		expect(test_contact.name).to eq "Carolyn Huynh"
 	end
 end
