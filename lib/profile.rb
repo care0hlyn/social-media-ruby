0273df2d0f773b0e5b291bf9671afd2473f25449
Carolyn Huynh <ckhuynh20@gmail.com>
2014-08-05 22:59:23 -0700

    create method to save contacts

--- lib/profile.rb
+++ lib/profile.rb
@@ -1,10 +1,19 @@
 class Profile
 
 	attr_reader :name, :twitter
+	@@contact = []
 
 	def initialize(attributes)
 		@name = attributes[:name]
 		@twitter = []
+	end
+
+	def Profile.all
+		@@contact
+	end
+
+	def save
+		@@contact << self
 	end
 
 end
--- spec/sm_spec.rb
+++ spec/sm_spec.rb
@@ -4,12 +4,20 @@ require 'username'
 
 describe Profile do
 	it 'initalizes the name of contact with an empty array for their Twitter username' do
-		test_contact = Profile.new({:name => "Carolyn Huynh"})
+		test_contact = Profile.new({:name => "Prez Obama"})
 		test_contact.should be_an_instance_of Profile
 	end
 
 	it 'shows the name of the contact' do
-		test_contact = Profile.new({:name => "Carolyn Huynh"})
-		expect(test_contact.name).to eq "Carolyn Huynh"
+		test_contact = Profile.new({:name => "Prez Obama"})
+		expect(test_contact.name).to eq "Prez Obama"
+	end
+
+	it 'saves the user contact' do
+		test_contact = Profile.new({:name => "Prez Obama"})
+		test_contact1 = Profile.new({:name => "Rick Astley"})
+		test_contact.save
+		test_contact1.save
+		expect(Profile.all).to eq [test_contact, test_contact1]
 	end
 end
